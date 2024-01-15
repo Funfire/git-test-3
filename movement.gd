bc5b2622df6653ecc90b2797a172c45846b081fc
Elon <Elonshagre@gmail.com>
2024-01-15 17:59:49 -0500

    aha
    ok

---
+++ movement.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+var gravity = 3
+var speed = 4
+var jump_speed =  6.0
+var mouse_sensitivity = 0.002
+
+func get_input():
+	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
+	velocity.x = input.x * speed
+	velocity.z = input.y * speed
+	
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	get_input()
+	move_and_slide()
+# Called when the node enters the scene tree for the first time.
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+	if event.is_action("jump") and is_on_floor():
+		velocity.y = jump_speed
