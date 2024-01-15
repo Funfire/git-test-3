2b8d232157823b9901de9b17933a1c4a984c9d5f
Elon <Elonshagre@gmail.com>
2024-01-15 18:51:03 -0500

    movement code

--- movement.gd
+++ movement.gd
@@ -1,26 +1,38 @@
 extends CharacterBody3D
 
-var gravity = 1
 var speed = 4
-var jump_speed =  6.0
+var gravity = -9.8
+var jump_speed = 6.0
 var mouse_sensitivity = 0.002
 @onready var camera = $Camera3D
 
-func get_input():
-	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
-	velocity.x = input.x * speed
-	velocity.z = input.y * speed
-	
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func get_input_direction():
+	var input_dir = Vector3.ZERO
+	input_dir.x = -Input.get_action_strength("strafe_left") + Input.get_action_strength("strafe_right")
+	input_dir.z = -Input.get_action_strength("move_forward") + Input.get_action_strength("move_back")
+	input_dir = input_dir.normalized()
+	return input_dir
+
 func _physics_process(delta):
-	velocity.y += -gravity * delta
-	get_input()
+	var input_dir = get_input_direction()
+	var rotation_dir = Vector3(input_dir.x, 0, input_dir.z)
+	rotation_dir = rotation_dir.rotated(Vector3.UP, rotation.y)
+
+	var horizontal_velocity = rotation_dir * speed
+	horizontal_velocity.y = velocity.y
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		horizontal_velocity.y = jump_speed
+	else:
+		horizontal_velocity.y += gravity * delta
+
+	velocity = horizontal_velocity
 	move_and_slide()
-# Called when the node enters the scene tree for the first time.
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
-		
-	if event.is_action("jump") and is_on_floor():
-		velocity.y = jump_speed
